6b8e967f74c976b02910aa0867ff3f9df894c80d
Luis David De La Cruz Bautista <delacruz_luis@outlook.es>
2018-12-13 17:42:28 -0600

    Implementación Android

---
+++ FingerForms/FingerForms/FingerForms.Android/FingerAuthDroid.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Security.Keystore;
+using Android.Views;
+using Android.Widget;
+using FingerForms.Droid;
+using Java.Security;
+using Javax.Crypto;
+using Xamarin.Forms;
+
+[assembly: Xamarin.Forms.Dependency(typeof(FingerAuthDroid))]
+namespace FingerForms.Droid
+{
+    public class FingerAuthDroid : IFingerService
+    {
+        private KeyStore keyStore;
+        private Cipher cipher;
+        private string KEY_NAME = "Ahsan";
+        public bool Auth()
+        {
+            try
+            {
+                cipher = Cipher.GetInstance(KeyProperties.KeyAlgorithmAes
+                    + "/"
+                    + KeyProperties.BlockModeCbc
+                    + "/"
+                    + KeyProperties.EncryptionPaddingPkcs7);
+                keyStore.Load(null);
+                IKey key = (IKey)keyStore.GetKey(KEY_NAME, null);
+                cipher.Init(CipherMode.EncryptMode, key);
+                return true;
+            }
+            catch (Exception ex) { return false; }
+        }
+    }
+}
---
+++ FingerForms/FingerForms/FingerForms/OtherMainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FingerForms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OtherMainPage : TabbedPage
+    {
+        public OtherMainPage ()
+        {
+            InitializeComponent();
+        }
+    }
+}
